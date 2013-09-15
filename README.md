8544c730609961a464a1c0927b114686f3754417
Lukas Stabe <lukas@stabe.de>
2013-09-15 03:27:39 +0200

    Initial code commit

--- README.md
+++ README.md
@@ -2,3 +2,56 @@ jekyll-menu
 ===========
 
 A Jekyll plugin that helps you generate nested and sorted menus.
+
+
+## Installation
+
+The following guide assumes Jekyll allows symlinks in _includes, whcih it does not currently do. This issue is tracked [here](https://github.com/mojombo/jekyll/issues/1552).
+
+For the time being, just copy the file `menu.html` instead of symlinking it. 
+
+### Your site is under version control with git
+
+Open up a terminal and change to your sites root.
+Then add a git submodule in the folder `_vendor` and symlink `menu.html` in `_includes` and `menu_generator.rb` in `_plugins`.
+
+tl;dr:
+
+```bash
+mkdir _vendor
+cd _vendor
+git submodule add https://github.com/Ahti/jekyll-menu.git
+cd ../_includes
+ln -s ../_vendor/jekyll-menu/menu.html
+cd ../_plugins
+ln -s ../_vendor/jekyll-menu/menu_generator.rb
+```
+
+### Your site is not under version control or not using git
+
+Start using git.
+
+> But I don't want to!
+
+Then at least check out this repo into _vendor and follow the steps outlined above.
+
+Instead of
+
+    git submodule add https://github.com/Ahti/jekyll-menu.git
+    
+use
+
+    git clone https://github.com/Ahti/jekyll-menu.git
+
+> I really don't want to use git at all >:(
+
+If you insist, just download `menu.html` and `menu_generator.rb` and put them into your `_includes` and `_plugins` folder respectively
+
+## Usage
+
+In any of your layouts, insert this code anywhere. I would recommend putting it inside a nav-tag:
+
+    {% include menu.html menu=site.menu %}
+    
+To include pages in this menu, include 
+
---
+++ menu_generator.rb
@@ -0,0 +1,127 @@
+require 'pp'
+
+module Jekyll
+    class Page
+        def menu
+            self.data['menu'] ||= {}
+            self.data['menu']
+        end
+        
+        def menu_parent
+            menu['parent']
+        end
+        
+        def menu_name
+            unless menu['name'].nil?
+                menu['name']
+            else
+                self.data['title']
+            end
+        end
+        
+        def subpages
+            self.data['subpages'] ||= []
+            self.data['subpages']
+        end
+    end
+    
+    
+    
+    class MenuGenerator < Generator
+        safe true
+        
+        def menu_name(hash)
+            hash['menu']['name'] || hash['title']
+        end
+        
+        def generate(site)
+            @pages = site.pages.dup
+            
+            @main_menu = []
+            @lookup = { 'main' => @main_menu }
+            
+            build_tree
+            sort_pages
+            generate_suburls
+            
+            site.config['menu'] = @main_menu
+        end
+        
+        def build_tree
+            # build the subpage tree
+            loop do 
+                prev_size = @pages.size
+                
+                @pages.reject! do |page|
+                    parent = @lookup[page.menu_parent]
+                    unless parent.nil?
+                        liquid_hash = page.to_liquid
+                        parent << liquid_hash
+                        liquid_hash['subpages'] ||= []
+                        @lookup[page.menu_name] = liquid_hash['subpages']
+                        true
+                    else
+                        false
+                    end
+                end
+                
+                break if @pages.size == prev_size
+            end
+        end
+        
+        def sort_pages
+            # lookup contains every page's subpage array,
+            # so we only need to sort every value in lookup
+            @lookup.each do |key, val|
+                val.sort! do |a, b|
+                    compare_pages(a, b)  
+                end
+            end
+        end
+        
+        def compare_pages(a, b)
+            if a['menu']['position'].nil? and b['menu']['position'].nil? or a['menu']['position'] == b['menu']['position']
+                # neither has a position, sort by name
+                menu_name(a) <=> menu_name(b)
+            elsif a['menu']['position'].nil?
+                # if a has no position, it goes after b
+                +1
+            elsif b['menu']['position'].nil?
+                # if b has no position, it goes after a
+                -1
+            else
+                # both have a position, compare them
+                a['menu']['position'] <=> b['menu']['position']
+            end  
+        end
+        
+        def generate_suburls
+            # generate the suburl lists.
+            # #set_suburls recurses, so we only call this
+            # on the pages in the main menu
+            @main_menu.each do |page|
+                set_suburls(page)
+            end
+        end
+        
+        def set_suburls(page)
+            page['subpages'].each do |subpage|
+                set_suburls(subpage)
+            end
+            page['suburls'] = suburls(page)
+        end
+        
+        def suburls(page_hash, add_self=false)
+            subsub = page_hash['subpages'].map do |subpage|
+                suburls(subpage, true)
+            end
+            subsub.flatten!
+            if add_self
+                subsub << page_hash['url']
+            else
+                subsub
+            end
+        end
+    end
+    
+end
